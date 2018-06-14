31b2736118a19b6d0073e2d68facc6df4ee10767
Gaston Longhitano <gaston@juntacorp.com>
2018-06-14 18:23:31 -0400

    feat: Code splitting
    Code splitting added using react-loadable
    - example component created to poc the code splitting

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -2,7 +2,7 @@ require('ignore-styles');
 
 require('babel-register')({
     ignore: [ /(node_modules)/ ],
-    presets: ['es2015', 'react-app']
+    presets: ['es2015', 'react-app'],
 });
 
 require('./index');
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Loadable from 'react-loadable'
+
+const AsyncComponent = Loadable({
+	loader: () => import("./Video"),
+	loading: () => <div>loading...</div>
+})
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        this is a server rendered
+        <AsyncComponent/>
       </div>
     );
   }
